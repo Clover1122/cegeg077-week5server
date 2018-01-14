f0a951bdefb8b4154754bba27d0d8af881f459d1
Claire Ellul <c.ellul@ucl.ac.uk>
2018-01-14 07:42:55 -0500

    added additional code to allow cross-origin requests in the server so that we can also serve static files to a phonegap app running on a different server or port

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 // express is the server that forms part of the nodejs program
 var express = require('express');
+
+
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // serve static files - e.g. html, css
 app.use(express.static(__dirname));
+
 
 var https = require('https');
 var fs = require('fs');
@@ -11,6 +20,7 @@ var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toS
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
+
 
 // set up some code to actually process requests from the client
 // the /  indicates what happens when the user types in https://developer.cege.ucl.ac.uk:31060/
