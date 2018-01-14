91d911be7d71202c5a07f20e6eceff62dc904055
Claire Ellul <c.ellul@ucl.ac.uk>
2018-01-14 06:39:16 -0500

    added a direct express static function into the server and a set of sub directories and test files to be used for testing purposes

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var app = express();
+// serve static files - e.g. html, css
+app.use(express.static(__dirname));
 
 var https = require('https');
 var fs = require('fs');
-var app = express();
 var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toString();
 var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString(); 
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
 
+// set up some code to actually process requests from the client
+// the /  indicates what happens when the user types in https://developer.cege.ucl.ac.uk:31060/
 app.get('/', function (req, res) {
-  // run some server-side code
-	console.log('the server has received a request'); 
-	res.send('Hello World');
+	// run some server-side code here - e.g. connect to a database and get data
+  
+	// console.log types information out to the terminal window (server)
+	console.log('the https server has received a request'); 
+	
+	// res.send sends text out to the browser (client)
+	res.send('Hello World - this is an https server');
 });
-app.get('/:fileName', function (req, res) {
-  // run some server-side code
-  	var fileName = req.params.fileName;
-	console.log(fileName + ' requested'); 
-	// note that __dirname  gives the path to the server.js file
-	res.sendFile(__dirname + '/'+ fileName);
-});
+
